819230fa19ac98ddc716f550cca65bdcb0a0e013
Khun-Cho-Lwin <65145625+Khun-Cho-Lwin@users.noreply.github.com>
2020-06-24 00:31:36 -0700

    Change Code

--- pqueue.py
+++ pqueue.py
@@ -24,28 +24,3 @@ class PQueue:
         for key in self.items.keys():
             size = size + self.items[key].size()
             return size
-
-from pqueue import PQueue
-p = PQueue()
-p.enqueue("HELLO",1)
-p.enqueue("H",2)
-p.enqueue("E",5)
-p.enqueue("L",3)
-p.enqueue("O",3)
-p.enqueue("Sample",9)
-# 7
-print(p.size())
-#Sample
-print(p.dequeue())
-#E
-print(p.dequeue())
-#4
-print(p.size())
-#L
-print(p.dequeue())
-#O
-print(p.dequeue())
-#H
-print(p.dequeue())
-#1
-print(p.size())
