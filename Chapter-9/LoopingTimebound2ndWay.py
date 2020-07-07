fa5561e5b59f1c8a0427f894cbbb45db6304856e
Khun-Cho-Lwin <khuncholwin.2019@gmail.com>
2020-07-07 10:17:51 -0700

    Add Files from Chapter-8

--- Chapter-9/LoopingTimebound2ndWay.py
+++
@@ -1,8 +0,0 @@
-import time
-def sum_of_n(n):
-    start = time.time()
-    the_sum = n * (n+1) / 2
-    end = time.time()
-    return the_sum,end-start
-for i in range(5):
-    print("Sum is %d , %.7f seconds" % sum_of_n(100000))
